e33318526044905290f3473a1cbac3e86180c02c
GiorginoSerbuciano <64908978+GiorginoSerbuciano@users.noreply.github.com>
2020-06-24 17:05:55 +0200

    Update d_2_filelessScan.py
    Compacted line 14 with a 'not in' statement; added notes.

--- d_2_filelessScan.py
+++ d_2_filelessScan.py
@@ -5,22 +5,22 @@ tagList = ['A_','B_','C_']
 
 fileDir = 'Files'
 fileList = os.scandir(fileDir)
-nameList = []
+nameList = [] #this list is read for the file names scanned in os.scandir(fileDir)
 
-for item in fileList:
-	if item.is_file():
+for item in fileList: #for every item resulting from os.scandir(fileDir)...
+	if item.is_file(): 
 		nameList.append(item.name)
-		print(item.name)
-	elif item.is_dir() and item.name != tagList[0] and item.name != tagList[1]  and item.name != tagList[2]:
+		print(item.name) #testing feature
+	elif item.is_dir() and item.name not in tagList: #if the destination folder is in the source folder and has the same name as its tag, the program will try to move it into itself, throwing up an error.
 		nameList.append(item.name)
-		print(item.name)
+		print(item.name) #testing feature
 
-for fileName in nameList:
-	if fileName.__contains__('A_'):
+for fileName in nameList: #reads every item in the list defined at the top
+	if fileName.__contains__('A_'): #tags and directories should be user-definable without having to edit the source code
 		shutil.move('Files/%s' %(fileName), 'Files/A_/%s' %(fileName))
-	elif fileName.__contains__('B_'):
-		print(fileName)
+	elif fileName.__contains__('B_'): #the program should check filenames for as many tags as there are tags; therefore, it should be able to 'generate' an elif (or something similar) for every tag.
+		print(fileName) #testing feature
 		shutil.move('Files/%s' %(fileName), 'Files/B_/%s' %(fileName))
-	elif fileName.__contains__('C_'):
-		print(fileName)
+	elif fileName.__contains__('C_'): 
+		print(fileName) #testing feature
 		shutil.move('Files/%s' %(fileName), 'Files/C_/%s' %(fileName))
