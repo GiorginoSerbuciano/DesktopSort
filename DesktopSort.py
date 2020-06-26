4dd09051b517e41416f7d9e31405d9f3f702ac63
GiorginoSerbuciano <64908978+GiorginoSerbuciano@users.noreply.github.com>
2020-06-26 18:40:34 +0200

    Fourth version
    Multiple source and destination directories supported. Tags and directories stored in text files.

--- DesktopSort.py
+++ DesktopSort.py
@@ -1,35 +1,21 @@
 import os
 import shutil
-
-sortDirFile = open('sortDirs.txt', 'r')
-sortDirList = sortDirFile.read().splitlines()
-print(sortDirList) #test
-
-tagList = ['A_','B_'] #for loop!
-
-fileDir = 'C:\\Users\\Serban\\Desktop\\%s'
-fileList = os.scandir(fileDir %(''))
-nameList = [] #this list is read for the file names scanned in os.scandir(fileDir)
-
-for item in fileList: #for every item resulting from os.scandir(fileDir)...
-	if item.is_file(): 
-		nameList.append(item.name)
-	elif item.is_dir() and item.name not in tagList: #if the destination folder is in the source folder and has the same name as its tag, the program will try to move it into itself, throwing up an error.
-		nameList.append(item.name)
-
-for sortDir in sortDirList:
-	if sortDir == sortDirList[0]:
-		for fileName in nameList: #reads every item in the list defined at the top
-			if fileName.__contains__(tagList[0]): #tags and directories should be user-definable without having to edit the source code
-				shutil.move(fileDir %(fileName), sortDirList[0] %(fileName))
-				print(fileName) #testing feature
-	elif sortDir == sortDirList[1]:
-		for fileName in nameList:
-			if fileName.__contains__(tagList[1]): #the program should check filenames for as many tags as there are tags; therefore, it should be able to 'generate' an elif (or something similar) for every tag
-				shutil.move(fileDir %(fileName), sortDirList[1] %(fileName))
-				print(fileName) #testing feature
-    
-
-
+tag_text = open('sortTags.txt', 'r')
+tag_list = tag_text.read().splitlines()
+print(tag_list)
+to_text = open('sortDirs.txt', 'r')
+to_list = to_text.read().splitlines()
+print(to_list)
+from_text = open('sourceDirs.txt', 'r')
+from_list = from_text.read().splitlines()
+from_dirscan = []
+for directory in from_list:
+	for item_entry in os.scandir(directory %('')):
+		from_dirscan.append(item_entry)
+for item in from_dirscan:
+	if item.name.__contains__(tag_list[0]):
+		shutil.move(item, to_list[0] %(item.name))
+	elif item.name.__contains__(tag_list[1]):
+		shutil.move(item, to_list[1] %(item.name))
 
 
---
+++ README.txt
@@ -0,0 +1,13 @@
+TAGS:
+tag_text: Textfile containing tags
+tag_list: List containing tags from tag_text
+DESTINATION DIRECTORIES:
+to_text: Textfile containing destination directories.
+to_list: List containing dest. dirs. from to_text.
+SOURCE DIRECTORIES:
+from_text: Textfile containing source directories.
+from_list: List containing source directories from from_list.
+from_dirscan: List containing the names of all files scanned in the source directories.
+
+
+source_dir_scan: os.scandir of 
